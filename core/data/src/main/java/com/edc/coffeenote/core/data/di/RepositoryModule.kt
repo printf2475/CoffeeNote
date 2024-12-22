5d54d982c5e7b61477be0b4db447d3d17864dd60
Jaewo <printf2475@naver.com>
2024-12-22 19:18:55 +0900

    :art: core:data CoffeeNoteRepository DI 추가

--- core/data/src/main/java/com/edc/coffeenote/core/data/di/RepositoryModule.kt
+++ core/data/src/main/java/com/edc/coffeenote/core/data/di/RepositoryModule.kt
@@ -1,14 +1,21 @@
 package com.edc.coffeenote.core.data.di
 
+import com.edc.coffeenote.core.data.repository.CoffeeNoteRepository
+import com.edc.coffeenote.core.data.repository.DefaultCoffeeNoteRepository
 import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import javax.inject.Singleton
 
-
 @Module
 @InstallIn(SingletonComponent::class)
 internal interface RepositoryModule {
+
+    @Binds
+    @Singleton
+    fun bindsCoffeeNoteRepository(
+        coffeeNoteRepository: DefaultCoffeeNoteRepository
+    ): CoffeeNoteRepository
 
 }
