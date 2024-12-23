1f7e953f2a51d798e37dade7a17c03dd0a585fec
Jaewo <printf2475@naver.com>
2024-12-23 13:14:27 +0900

    :art: core:database TestDaoModule, DaoModule 통합

--- core/database/src/main/java/com/edc/database/di/DaoModule.kt
+++ core/database/src/main/java/com/edc/database/di/DaoModule.kt
@@ -1,11 +1,15 @@
 package com.edc.database.di
 
+import android.content.Context
+import androidx.room.Room
 import com.edc.database.CoffeeNoteDataBase
 import com.edc.database.dao.CoffeeNoteDao
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import javax.inject.Named
 import javax.inject.Singleton
 
 
@@ -17,4 +21,15 @@ internal object DaoModule {
     fun providesCoffeeNoteDao(
         database: CoffeeNoteDataBase,
     ): CoffeeNoteDao = database.coffeeNoteDao()
+
+    @Provides
+    @Singleton
+    @Named("test_coffee_note_dao")
+    fun providesTestCoffeeNoteDao(
+        @ApplicationContext context: Context
+    ): CoffeeNoteDao =
+        Room.inMemoryDatabaseBuilder(
+            context = context,
+            klass = CoffeeNoteDataBase::class.java,
+        ).build().coffeeNoteDao()
 }
