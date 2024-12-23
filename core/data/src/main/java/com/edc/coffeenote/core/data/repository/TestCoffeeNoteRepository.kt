bea2d108797f433ab88f6d2fbec6fbe933345e9a
Jaewo <printf2475@naver.com>
2024-12-23 13:15:25 +0900

    :art: core:data TestCoffeeNoteRepository 추가

---
+++ core/data/src/main/java/com/edc/coffeenote/core/data/repository/TestCoffeeNoteRepository.kt
@@ -0,0 +1,27 @@
+package com.edc.coffeenote.core.data.repository
+
+import com.edc.coffeenote.core.model.CoffeeNote
+import com.edc.database.dao.CoffeeNoteDao
+import com.edc.database.mapper.toEntity
+import com.edc.database.mapper.toModel
+import com.edc.database.model.CoffeeNoteEntity
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
+import javax.inject.Inject
+import javax.inject.Named
+
+class TestCoffeeNoteRepository @Inject constructor(
+    @Named("test_coffee_note_dao") private val coffeeNoteDao: CoffeeNoteDao
+) : CoffeeNoteRepository {
+    override fun getAllCoffeeNote(): Flow<List<CoffeeNote>> =
+        coffeeNoteDao.getAll().map { it.map(CoffeeNoteEntity::toModel) }
+
+    override fun getCoffeeNoteById(id: String): Flow<CoffeeNote> =
+        coffeeNoteDao.getById(id).map(CoffeeNoteEntity::toModel)
+
+    override suspend fun upsertCoffeeNote(coffeeNote: CoffeeNote) =
+        coffeeNoteDao.upsert(coffeeNote.toEntity())
+
+    override suspend fun deleteCoffeeNote(id: String) =
+        coffeeNoteDao.deleteById(id)
+}
