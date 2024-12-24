0b5ae33e39e13de491cca43ccf4db09fe1af625d
Jaewo <printf2475@naver.com>
2024-12-24 13:08:52 +0900

    :art: core:database CoffeeNoteEntity 모델 데이터 수정

--- core/database/src/main/java/com/edc/database/model/CoffeeNoteEntity.kt
+++ core/database/src/main/java/com/edc/database/model/CoffeeNoteEntity.kt
@@ -3,16 +3,19 @@ package com.edc.database.model
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import kotlinx.collections.immutable.PersistentList
 import java.util.UUID
 
 @Entity(tableName = "coffee_note_entity")
 data class CoffeeNoteEntity(
     @PrimaryKey
     val id: String = UUID.randomUUID().toString(),
-    @ColumnInfo(name = "coffee_name")
-    val coffeeName: String,
+    @ColumnInfo("been_info")
+    val beenInfo: BeenInfoEntity,
     @ColumnInfo(name = "brewing_recipe")
     val brewingRecipe: List<BrewingStepEntity>,
+    @ColumnInfo("flavor_point")
+    val flavorPoint: Int? = null,
     val notes: String? = null,
     val date: String
 )
@@ -20,6 +23,7 @@ data class CoffeeNoteEntity(
 data class BrewingStepEntity(
     val type: BrewingTypeEntity,
     val time: Int = type.defaultTime,
+    @ColumnInfo("amount_of_water")
     val amountOfWater: Int? = null,
     val description: String? = null
 )
@@ -28,4 +32,14 @@ enum class BrewingTypeEntity(val defaultTime: Int) {
     Bloom(30),
     Pouring(10),
 }
+
+data class BeenInfoEntity(
+    @ColumnInfo("been_name")
+    val beenName: String,
+    val roastery: String? = null,
+    @ColumnInfo("flavor_notes")
+    val flavorNotes: PersistentList<String>? = null,
+    @ColumnInfo("roasting_point")
+    val roastingPoint: Int? = null,
+)
 
