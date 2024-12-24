b44c1319ffff3dfefa53dfe110ce1b4b51c73e9f
Jaewo <printf2475@naver.com>
2024-12-24 13:08:28 +0900

    :art: core:model CoffeeNote 모델 데이터 수정

--- core/model/src/main/java/com/edc/coffeenote/core/model/CoffeeNote.kt
+++ core/model/src/main/java/com/edc/coffeenote/core/model/CoffeeNote.kt
@@ -1,11 +1,20 @@
 package com.edc.coffeenote.core.model
 
+import kotlinx.collections.immutable.PersistentList
 import java.util.UUID
 
 data class CoffeeNote(
     val id: String = UUID.randomUUID().toString(),
-    val coffeeName: String,
+    val beenInfo: BeenInfo,
     val brewingRecipe: List<BrewingStep>,
+    val flavorPoint: Int? = null,
     val notes: String? = null,
     val date: String
+)
+
+data class BeenInfo(
+    val beenName: String,
+    val roastery: String? = null,
+    val flavorNotes: PersistentList<String>? = null,
+    val roastingPoint: Int? = null,
 )
