fb91661d8b384e0044b56ca48c3ed60137249153
Jaewo <printf2475@naver.com>
2025-01-15 00:09:03 +0900

    :art: core:data CoffeeNoteRepository 구현체 수정

--- core/data/src/main/java/com/edc/coffeenote/core/data/repository/DefaultCoffeeNoteRepository.kt
+++ core/data/src/main/java/com/edc/coffeenote/core/data/repository/DefaultCoffeeNoteRepository.kt
@@ -4,7 +4,7 @@ import com.edc.coffeenote.core.model.CoffeeNote
 import com.edc.database.dao.CoffeeNoteDao
 import com.edc.database.mapper.toEntity
 import com.edc.database.mapper.toModel
-import com.edc.database.model.CoffeeNoteEntity
+import com.edc.database.model.CoffeeNoteWithBeenInfo
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
 import javax.inject.Inject
@@ -13,14 +13,14 @@ internal class DefaultCoffeeNoteRepository @Inject constructor(
     private val coffeeNoteDao: CoffeeNoteDao
 ) : CoffeeNoteRepository {
     override fun getAllCoffeeNote(): Flow<List<CoffeeNote>> =
-        coffeeNoteDao.getAll().map { it.map(CoffeeNoteEntity::toModel) }
+        coffeeNoteDao.getCoffeeNoteAll().map { it.map(CoffeeNoteWithBeenInfo::toModel) }
 
     override fun getCoffeeNoteById(id: String): Flow<CoffeeNote> =
-        coffeeNoteDao.getById(id).map(CoffeeNoteEntity::toModel)
+        coffeeNoteDao.getCoffeeNoteById(id).map(CoffeeNoteWithBeenInfo::toModel)
 
     override suspend fun upsertCoffeeNote(coffeeNote: CoffeeNote) =
-        coffeeNoteDao.upsert(coffeeNote.toEntity())
+        coffeeNoteDao.upsertCoffeeNote(coffeeNote.toEntity())
 
     override suspend fun deleteCoffeeNote(id: String) =
-        coffeeNoteDao.deleteById(id)
+        coffeeNoteDao.deleteCoffeeNoteById(id)
 }
